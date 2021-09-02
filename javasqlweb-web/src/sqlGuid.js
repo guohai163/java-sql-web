947ec69ef96bed143ecb4f055f693cce9a198206
guohai <guohai@gyyx.cn>
2021-09-02 15:12:23 +0800

    增加分类和复制功能

--- javasqlweb-web/src/sqlGuid.js
+++ javasqlweb-web/src/sqlGuid.js
@@ -1,19 +1,21 @@
 
 import React from "react";
 import './sqlGuid.css';
-import { Layout, Menu, Breadcrumb,List,Avatar,Image } from 'antd';
+import { Layout, Menu, List, Button, message, Image } from 'antd';
 import sqlicon from './images/sql-svgrepo-com.svg'
 import SyntaxHighlighter from 'react-syntax-highlighter';
 import { docco } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 import FetchHttpClient, { json } from 'fetch-http-client';
 import config from "./config";
+import copy from 'copy-to-clipboard';
 const { Header, Content  } = Layout;
 
 class SqlGuid extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            guidData: []
+            guidData: [],
+            menuKey: "stage"
         }
     }
     componentDidMount() {
@@ -27,21 +29,33 @@ class SqlGuid extends React.Component {
 
         })
     }
+    menuSwitch(menu) {
+        console.log(menu.key)
+        this.setState({
+            menuKey: menu.key
+        })
+    }
+    copyText(o) {
+        console.log(o)
+        copy(o)
+        message.info('复制成功');
+    }
     render(){
-        const {guidData} = this.state;
+        const {guidData, menuKey} = this.state;
         return(
             <Layout>
                 <Header className="header">
-                    <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
-                        <Menu.Item key="1">社区类</Menu.Item>
+                    <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['stage']} onClick={this.menuSwitch.bind(this)}>
+                        <Menu.Item key="stage">社区类</Menu.Item>
+                        <Menu.Item key="cospower">电池类</Menu.Item>
                     </Menu>
                 </Header>
                 <Content className="site-layout" style={{ padding: '0 50px', marginTop: 64 }}>
                     <List
                         itemLayout="horizontal"
 
-                        dataSource={guidData}
-                        renderItem={item => <List.Item><List.Item.Meta
+                        dataSource={guidData.filter(item => item.category === menuKey)}
+                        renderItem={item => <List.Item actions={[<Button size="small" type="primary" onClick={this.copyText.bind(this, item.script)}>copy</Button>]}><List.Item.Meta
                             avatar=<Image src={sqlicon} alt="logo" width={50} />
                             title={item.title + "，服务器【"+item.server+"】数据库【"+item.database+"】"}
                             description=<SyntaxHighlighter language="sql" style={docco}>
