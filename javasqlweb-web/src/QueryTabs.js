b48f09e172b37476bf1917b0a7e676e6590d6da9
H!Guo <guohai@gmail.com>
2022-02-10 23:41:25 +0800

    尝试增加标签页

---
+++ javasqlweb-web/src/QueryTabs.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Tabs } from 'antd';
+import PageContent from "./PageContent";
+const {TabPane } = Tabs;
+
+const initialPanes = [
+    {title: 'Tab 1', sql: '', data: [], key: '1'}
+];
+class QueryTabs extends React.Component {
+    state = {
+        activeKey: initialPanes[0].key,
+        panes: initialPanes
+    };
+
+    render() {
+        const {panes, activeKey} = this.state;
+        return(
+            <Tabs></Tabs>
+        );
+    }
+}
+
+export default QueryTabs
